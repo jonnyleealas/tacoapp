ccf4e71205281924d816d968205310ca52a8ca14
Jonathon Lee <jonnyleealas@gmail.com>
2024-06-09 21:34:35 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.send('<h1>hello</h1>')
 })
 
+app.post('/', (req, res) => {
+    res.send('post to home')
+})
+
+app.delete('/delete', (req, res) => {
+    res.send("delete")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`listening on ${process.env.PORT} `)
 })
