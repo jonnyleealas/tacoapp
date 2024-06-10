2ed05600441257f5ac1a1bbfc3ac51ae1f7935a3
Jonathon Lee <jonnyleealas@gmail.com>
2024-06-09 20:43:10 -0500

    removed app.css

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>hello world</h1>
-    </>
+    <div className='text-3xl font-bold underline'>App</div>
   )
 }
 
-export default App
